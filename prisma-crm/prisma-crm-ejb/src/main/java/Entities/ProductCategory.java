e19075d1dc98c6ddb4b058e71e18e2f8357373e6
nada <nada.baklouti@esprit.tn>
2019-10-18 19:34:23 +0100

    product category store entities

--- prisma-crm/prisma-crm-ejb/src/main/java/Entities/ProductCategory.java
+++ prisma-crm/prisma-crm-ejb/src/main/java/Entities/ProductCategory.java
@@ -29,5 +29,53 @@ public class ProductCategory implements Serializable{
 	
 	@OneToMany(cascade = CascadeType.ALL, mappedBy="productCategory")
 	private Set<Product> products;
+
+	public ProductCategory() {
+		super();
+	}
+
+	public ProductCategory(String name, Set<Product> products) {
+		super();
+		this.name = name;
+		this.products = products;
+	}
+
+	public ProductCategory(int id, String name, Set<Product> products) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.products = products;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(Set<Product> products) {
+		this.products = products;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	
+	
 	
 }
--- prisma-crm/prisma-crm-ejb/src/main/java/Entities/Store.java
+++ prisma-crm/prisma-crm-ejb/src/main/java/Entities/Store.java
@@ -26,5 +26,70 @@ public class Store implements Serializable{
 	@Column(name = "address")
 	private String address;
 	
+	@Column(name = "telephone")
+	private String telephone;
+
+	public Store() {
+		super();
+	}
+
+	public Store(String name, String address, String telephone) {
+		super();
+		this.name = name;
+		this.address = address;
+		this.telephone = telephone;
+	}
+
+	public Store(int id, String name, String address, String telephone) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.address = address;
+		this.telephone = telephone;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
