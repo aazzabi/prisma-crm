06e865b13f36b992ce9f300f3aa610f2275e09f5
Arafet AZZABI <arafet.azzabi@esprit.tn>
2019-10-18 19:19:18 +0200

    Claim + NoteClaim entities

---
+++ prisma-crm/prisma-crm-ejb/src/main/java/Entities/NoteClaim.java
@@ -0,0 +1,82 @@
+package Entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import Enums.*;
+
+@Entity
+public class NoteClaim implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.TABLE)
+	private int id;
+	
+	@Column
+	private String description;
+
+	@OneToMany
+	private User createdBy;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date createdAt;
+	
+	@ManyToOne
+	private Claim claim;
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public User getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(User createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Claim getClaim() {
+		return claim;
+	}
+
+	public void setClaim(Claim claim) {
+		this.claim = claim;
+	}
+	
+	
+}
